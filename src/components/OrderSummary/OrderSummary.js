7ea22ff98d53166c8ad78e6940c1d772c1172ce1
Viktor <Viktor>
2024-03-01 16:24:49 +0000

    fix bug

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -7,6 +7,59 @@ import CurrencyFormatter from '../CurrencyFormatter';
 
 import * as styles from './OrderSummary.module.css';
 
+import axios from 'axios';
+
+
+const useMonoBankPayment = () => {
+  const handlePayMono = async (email: string, amount: number) => {
+    const monoData = await axios
+      .post(
+        'https://api.monobank.ua/api/merchant/invoice/create',
+        {
+          amount: amount * 100,
+          ccy: 840,
+          redirectUrl: 'https://amanita-store.com/shop/',
+          merchantPaymInfo: {
+            customerEmails: [email], // Масив пошт, на які потрібно відправити фіскальний чек, якщо у мерчанта активна звʼязка з checkbox
+            destination: 'Покупка речей',
+            basketOrder: [
+              // Склад замовлення, використовується для відображення кошика замовлення, обовʼязково вказувати при активній звʼязці з ПРРО (звʼязка створюється у веб-кабінеті https://web.monobank.ua або через портал check by mono https://www.monobank.ua/check)
+              // {
+              //   name: 'Табуретка',
+              //   qty: 2,
+              //   sum: 2100,
+              //   // icon: 'string',
+              //   // unit: 'шт.',
+              //   // code: 'd21da1c47f3c45fca10a10c32518bdeb',
+              //   // barcode: 'string',
+              //   // header: 'string',
+              //   // footer: 'string',
+              //   tax: [],
+              //   uktzed: 'string',
+              //   discounts: [],
+              // },
+            ],
+          },
+        },
+        {
+          headers: {
+            'X-Token': process.env.GATSBY_MONO_TOKEN,
+          },
+        }
+      )
+      .catch((e) => console.log('MMM error', e));
+
+
+    if (monoData) window.location.href = monoData.data.pageUrl;
+    // window.open(monoData?.data.pageUrl);
+  };
+
+  return handlePayMono;
+};
+
+export default useMonoBankPayment;
+
+
 const OrderSummary = (props) => {
   const [coupon, setCoupon] = useState('');
   const [giftCard, setGiftCard] = useState('');

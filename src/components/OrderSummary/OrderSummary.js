9ff6c60de0b9320562ee0a3f119bdd0f83cd118c
Viktor <Viktor>
2024-03-13 22:11:17 +0000

    fix images

--- src/components/OrderSummary/OrderSummary.js
+++ src/components/OrderSummary/OrderSummary.js
@@ -5,56 +5,9 @@ import Button from '../Button';
 import FormInputField from '../FormInputField/FormInputField';
 import CurrencyFormatter from '../CurrencyFormatter';
 import sendDataToBot from '../../helpers/sendDataToBot';
+import useBankPayment from '../../hooks/useBankPayment';
 
 import * as styles from './OrderSummary.module.css';
-
-import axios from 'axios';
-
-const useMonoBankPayment = () => {
-  const handlePayMono = async (name, amount) => {
-    const monoData = await axios
-      .post(
-        'https://api.monobank.ua/api/merchant/invoice/create',
-        {
-          amount: amount * 100,
-          ccy: 840,
-          redirectUrl: 'https://amanita-store.com/orderConfirm',
-          merchantPaymInfo: {
-            customerEmails: [], // Масив пошт, на які потрібно відправити фіскальний чек, якщо у мерчанта активна звʼязка з checkbox
-            destination: `Покупка речей, ${name}`,
-            basketOrder: [
-              // Склад замовлення, використовується для відображення кошика замовлення, обовʼязково вказувати при активній звʼязці з ПРРО (звʼязка створюється у веб-кабінеті https://web.monobank.ua або через портал check by mono https://www.monobank.ua/check)
-              // {
-              //   name: 'Табуретка',
-              //   qty: 2,
-              //   sum: 2100,
-              //   // icon: 'string',
-              //   // unit: 'шт.',
-              //   // code: 'd21da1c47f3c45fca10a10c32518bdeb',
-              //   // barcode: 'string',
-              //   // header: 'string',
-              //   // footer: 'string',
-              //   tax: [],
-              //   uktzed: 'string',
-              //   discounts: [],
-              // },
-            ],
-          },
-        },
-        {
-          headers: {
-            'X-Token': 'm5s0kEPoyNYzoqL1aDISeSw',
-          },
-        }
-      )
-      .catch((e) => console.log('MMM error', e));
-
-    if (monoData) window.location.href = monoData.data.pageUrl;
-    // window.open(monoData?.data.pageUrl);
-  };
-
-  return handlePayMono;
-};
 
 const isTest = () => window.location.search.includes('mode=test');
 
@@ -67,10 +20,11 @@ const OrderSummary = (props) => {
   const [phone, setPhone] = useState('');
   const [address, setAddress] = useState('');
 
-  const handlePayMono = useMonoBankPayment();
+  const handlePay = useBankPayment();
 
   const handleBuy = () => {
-    sendDataToBot(`
+    false &&
+      sendDataToBot(`
       магазин одягу
 
       name: ${name}
@@ -83,7 +37,7 @@ const OrderSummary = (props) => {
 
       price: ${props.totalPrice}$
     `);
-    handlePayMono(name, isTest() ? 1 : props.totalPrice);
+    handlePay(name, isTest() ? 1 : props.totalPrice, email, 1);
   };
 
   return (

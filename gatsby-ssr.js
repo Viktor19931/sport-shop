4a199d206442ae1b9b5514847c2a970d085e5820
Viktor <Viktor>
2024-03-20 20:49:54 +0000

    f

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,10 +1,7 @@
 import React from 'react';
 
 import { CartProvider } from './src/context/cartContext';
-import { LocalizationProvider } from './src/context/localizationContext';
 
 export const wrapRootElement = ({ element }) => (
-  <LocalizationProvider>
-    <CartProvider>{element}</CartProvider>
-  </LocalizationProvider>
+  <CartProvider>{element}</CartProvider>
 );

a22f93021dc4233d6c8248e99af347b78596f8d1
Viktor <Viktor>
2024-03-23 22:01:56 +0100

    use const

--- src/hooks/useVostokPayment.js
+++ src/hooks/useVostokPayment.js
@@ -1,12 +1,13 @@
 import forge from 'node-forge';
 import CryptoJS from 'crypto-js';
 
+const ORDER_NUMBER = Date.now();
+const MERCHANT_id = process.env.GATSBY_VOSTOK_MERCHANT_ID;
+const AUTH_TYPE = 1;
+
 const useVostokPayment = () => {
   const handlePayVostok = async (name, amount, email, rate) => {
     const amountToPay = (amount * rate).toFixed(2);
-    const orderNumber = Date.now();
-    const merchantId = process.env.GATSBY_VOSTOK_MERCHANT_ID;
-    const authType = 1;
 
     // hash of private key
     // e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855
@@ -14,13 +15,7 @@ const useVostokPayment = () => {
     const privateKeyPem = process.env.GATSBY_VOSTOK_PRIVATE_KEY;
     const privateKeyHash = CryptoJS.SHA256().toString(CryptoJS.enc.Hex);
 
-    console.log(
-      'KKK isCorrectHash ',
-      privateKeyHash ===
-        'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'
-    );
-
-    const data = `${orderNumber}|${amountToPay}|${merchantId}|${authType}`;
+    const data = `${ORDER_NUMBER}|${amountToPay}|${MERCHANT_id}|${AUTH_TYPE}`;
 
     const privateKey = forge.pki.privateKeyFromPem(privateKeyPem);
 
@@ -35,12 +30,12 @@ const useVostokPayment = () => {
     eComPay.phoneNumber = '+380981234567';
     eComPay.amount = amountToPay;
     eComPay.description = 'Тестовий платіж';
-    eComPay.partnerOrderId = orderNumber;
-    eComPay.merchantId = merchantId;
-    eComPay.authType = authType;
+    eComPay.partnerOrderId = ORDER_NUMBER;
+    eComPay.merchantId = MERCHANT_id;
+    eComPay.authType = AUTH_TYPE;
     eComPay.successRedirectUrl = `https://elite-sport.netlify.app/orderConfirm?name=${name}&amount=${amount}`;
     eComPay.failureRedirectUrl = 'https://elite-sport.netlify.app/404';
-    eComPay.cultureName = 'uk-UA';
+    eComPay.cultureName = 'uk-UA'; // TODO use en
     eComPay.signature = base64Signature;
     eComPay.keyHash = privateKeyHash;
     eComPay.customParameters = {};
